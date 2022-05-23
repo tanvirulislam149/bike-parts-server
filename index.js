9422774156c2270657e353eedf20c6e9410314e0
unknown <tanvirulislam149@gmail.com>
2022-05-23 23:31:03 +0600

    all parts get done

--- index.js
+++ index.js
@@ -12,12 +12,31 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idgn7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-    console.log("db connected");
-});
+async function run() {
+    try {
+        await client.connect();
+
+        const partsCollection = client.db("autoParts").collection("parts");
+
+        app.get("/", async (req, res) => {
+            res.send("Auto Parts Company");
+        })
+
+
+        app.get("/parts", async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+
+
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
